66ccc26afd2090efd2d8d88eb2df3688fcde50ab
Franklin Javier <franklinjalves@gmail.com>
2021-11-02 09:11:56 -0300

    chore: prettier

--- server/nunjucks.js
+++ server/nunjucks.js
@@ -6,6 +6,9 @@ const debug = require('debug')('storefront:nunjucks')
 const APP_ROOT = require('path').join(__dirname + '/../')
 
 const FileLoaderInstance = new nunjucks.FileSystemLoader(APP_ROOT + '/views')
-const env = new nunjucks.Environment(FileLoaderInstance, { trimBlocks: true, lstripBlocks: true })
+const env = new nunjucks.Environment(FileLoaderInstance, {
+  trimBlocks: true,
+  lstripBlocks: true
+})
 
 module.exports = env
